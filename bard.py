56dd3fc97679e11219cec3f12e05b3046e7c7daf
$anket <88323848+Sanket09-star@users.noreply.github.com>
2023-07-04 22:55:43 +0530

    Update bard.py

--- bard.py
+++ bard.py
@@ -2,12 +2,12 @@ from bardapi import Bard
 '''
 For token :
 Go to the google bard 
-Sign in your account 
-Then go to inspect section
-Click on application then cookies
+Sign in to your account 
+Then go to inspect a section
+Click on the application then cookies
 then click on first link(https:bard.google...
 find in the 'Name' column '_Secure-1PSID' in that row copy Value of that _Secure-1PSID
-Paste that value in token..
+Paste that value in the token.
 '''
 token = 'xxxxxx'
 bard = Bard(token=token)
